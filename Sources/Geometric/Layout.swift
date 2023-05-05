47b050969b9911207c1d83df73bd6d5650fbcc8f
Jordan Kay <jordan@fleuronic.com>
2023-05-04 22:31:51 -0500

    Update layout

---
+++ Sources/Geometric/Layout.swift
@@ -0,0 +1,30 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+import UIKit
+import Layoutless
+
+import struct Metric.Styled
+
+public extension Styled {
+	var layout: Layout<UIView> {
+		.just(base)
+	}
+}
+
+// MARK: -
+public extension UIView {
+	static func containing(_ layout: AnyLayout) -> AnyLayout {
+		UIView().addingLayout(layout)
+	}
+}
+
+// MARK: -
+@resultBuilder public struct LayoutBuilder {
+	public static func buildBlock(_ layouts: AnyLayout?...) -> [AnyLayout] {
+		layouts.compactMap { $0 }
+	}
+
+	public static func buildBlock(_ layouts: [AnyLayout?]) -> [AnyLayout] {
+		layouts.compactMap { $0 }
+	}
+}
--- Sources/Geometric/View.swift
+++ Sources/Geometric/View.swift
@@ -1,22 +1,9 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import UIKit
-import Layoutless
 
-open class View<Base: UIView>: UIView {
-	open var layout: Layout<Base> {
-		fatalError()
-	}
-
-	// MARK: UIView
-	public override init(frame: CGRect) {
-		super.init(frame: frame)
-
-		layout.fillingParent().layout(in: self)
-	}
-
-	// MARK: NSCoding
-	public required init(coder: NSCoder) {
-		fatalError()
+public extension UIView {
+	static var spacer: Self {
+		.init()
 	}
 }
