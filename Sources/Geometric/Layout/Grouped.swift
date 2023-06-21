62fbc27a0e796dc3d67dea1d4eb686089701fc6c
Fleuronic <jordan@fleuronic.com>
2023-06-21 18:29:10 -0400

    Add Grouped

---
+++ Sources/Geometric/Layout/Grouped.swift
@@ -0,0 +1,9 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+import Layoutless
+
+@resultBuilder public struct Grouped {
+    public static func buildBlock(_ layouts: AnyLayout...) -> AnyLayout {
+        group(layouts)
+    }
+}
